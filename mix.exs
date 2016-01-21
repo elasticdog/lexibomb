24310d1e96318d6e3551ffb655edbec0ca3dcac0
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-01-20 16:00:08 -0800

    Add dialyxir as a dependency of the umbrella project
        mix deps.get
        mix deps.compile
        mix dialyzer.plt
        mix do compile, dialyzer

--- mix.exs
+++ mix.exs
@@ -21,6 +21,6 @@ defmodule Lexibomb.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [{:dialyxir, "~> 0.3", only: [:dev]}]
   end
 end
