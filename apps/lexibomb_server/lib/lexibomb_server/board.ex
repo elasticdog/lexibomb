e66463669aee8544c49b2cc6f8e61390c40ac394
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-01-26 14:10:58 -0800

    Draw the entire raw board when debugging
    The draw function has been renamed to debug to be more explicit in its
    usecase (and the term "draw" has connotations with drawing a tile). I've
    also set most of the helper functions to be private now that this is
    working.

--- apps/lexibomb_server/lib/lexibomb_server/board.ex
+++ apps/lexibomb_server/lib/lexibomb_server/board.ex
@@ -17,9 +17,11 @@ defmodule LexibombServer.Board do
     for _ <- 1..total_squares, do: %Square{}
   end
 
-  def draw(device \\ :stdio, board) do
-    size = size_of(board)
-    header = draw_header(size)
+  def debug(device \\ :stdio, board) do
+    size = size_of_raw(board)
+    border = 2
+
+    header = draw_header(size - border)
     top = draw_grid_line(size, :top)
     middle = draw_all_rows(board)
     bottom = draw_grid_line(size, :bottom)
@@ -31,25 +33,30 @@ defmodule LexibombServer.Board do
   def size_of(board) do
     border = 2
     board
-    |> length
-    |> :math.sqrt
-    |> round
+    |> size_of_raw
     |> Kernel.'-'(border)
   end
 
-  def draw_header(size) do
+  defp size_of_raw(board) do
+    board
+    |> length
+    |> :math.sqrt
+    |> round
+  end
+
+  defp draw_header(size) do
     last_label = ?a + size - 1
     header =
       Enum.to_list(?a .. last_label)
       |> List.to_string
       |> String.graphemes
       |> Enum.join("   ")
-    "     " <> header <> "\n"
+    "         " <> header <> "\n"
   end
 
-  def draw_grid_line(size, :top), do: do_draw_grid_line(size, "┌", "┬", "┐")
-  def draw_grid_line(size, :middle), do: do_draw_grid_line(size, "├", "┼", "┤")
-  def draw_grid_line(size, :bottom), do: do_draw_grid_line(size, "└", "┴", "┘")
+  defp draw_grid_line(size, :top), do: do_draw_grid_line(size, "┌", "┬", "┐")
+  defp draw_grid_line(size, :middle), do: do_draw_grid_line(size, "├", "┼", "┤")
+  defp draw_grid_line(size, :bottom), do: do_draw_grid_line(size, "└", "┴", "┘")
 
   defp do_draw_grid_line(size, first, inner, last) do
     separators = for _ <- 0..size-2, do: inner
@@ -58,30 +65,42 @@ defmodule LexibombServer.Board do
     "   " <> grid_line <> "\n"
   end
 
-  def draw_all_rows(board) do
-    size = size_of(board)
+  defp draw_all_rows(board) do
+    size = size_of_raw(board)
     grid_line = draw_grid_line(size, :middle)
-    rows = for row <- 1..size, do: draw_row(board, row)
+    rows = for row <- 0..size-1, do: draw_row(board, row)
     rows |> Enum.join(grid_line)
   end
 
-  def draw_row(board, row) do
-    label = zero_pad(row, 2)
+  defp draw_row(board, row) do
+    first_row = 0
+    last_row = size_of_raw(board) - 1
+    label =
+      case row do
+        ^first_row -> "  "
+        ^last_row -> "  "
+        _ -> zero_pad(row, 2)
+      end
+
     row =
       get_row(board, row)
       |> Enum.map(&Square.draw/1)
       |> Enum.join("│")
+
     label <> " │" <> row <> "│\n"
   end
 
-  def zero_pad(n, length) do
+  defp zero_pad(n, length) do
     n |> to_string |> String.rjust(length, ?0)
   end
 
-  def get_row(board, row) do
-    size = size_of(board)
-    border = 2
-    offset = row * (size + border)
+  defp get_row(board, row) do
+    size = size_of_raw(board)
+    offset =
+      case row do
+        0 -> 0
+        _ -> (row * size) - 1
+      end
     Enum.slice(board, offset, size)
   end
 end
