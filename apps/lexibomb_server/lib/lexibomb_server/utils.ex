d65a4f0f62a78a10f491c8d0736451d816c0d474
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-01-28 12:37:26 -0800

    Add typespecs for the Util functions

--- apps/lexibomb_server/lib/lexibomb_server/utils.ex
+++ apps/lexibomb_server/lib/lexibomb_server/utils.ex
@@ -3,9 +3,12 @@ defmodule LexibombServer.Utils do
   Helper utilities used throughout LexibombServer.
   """
 
+  @spec draw_grid_line(pos_integer, :top | :middle | :bottom) :: String.t
   def draw_grid_line(size, :top),    do: do_draw_grid_line(size, "┌", "┬", "┐")
   def draw_grid_line(size, :middle), do: do_draw_grid_line(size, "├", "┼", "┤")
   def draw_grid_line(size, :bottom), do: do_draw_grid_line(size, "└", "┴", "┘")
+
+  @spec do_draw_grid_line(pos_integer, String.t, String.t, String.t) :: String.t
 
   defp do_draw_grid_line(1, first, _, last) do
     Enum.join([first, "───", last])
@@ -17,6 +20,7 @@ defmodule LexibombServer.Utils do
     Enum.join(separators, "───")
   end
 
+  @spec draw_segments([String.t], String.t) :: String.t
   def draw_segments(strings, sep \\ "│") do
     sep <> Enum.join(strings, sep) <> sep
   end
@@ -33,6 +37,7 @@ defmodule LexibombServer.Utils do
     end
   end
 
+  @spec zero_pad(non_neg_integer, non_neg_integer) :: String.t
   def zero_pad(n, len) when is_integer(n) do
     n |> to_string |> String.rjust(len, ?0)
   end
