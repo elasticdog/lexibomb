b54d040fc53597b2f52655c857f9a02df0646e02
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-01-28 20:48:06 -0800

    Add item_width option when drawing grid lines
    This makes things slightly more generic.

--- apps/lexibomb_server/lib/lexibomb_server/utils.ex
+++ apps/lexibomb_server/lib/lexibomb_server/utils.ex
@@ -3,21 +3,41 @@ defmodule LexibombServer.Utils do
   Helper utilities used throughout LexibombServer.
   """
 
-  @spec draw_grid_line(pos_integer, :top | :middle | :bottom) :: String.t
-  def draw_grid_line(size, :top),    do: do_draw_grid_line(size, "┌", "┬", "┐")
-  def draw_grid_line(size, :middle), do: do_draw_grid_line(size, "├", "┼", "┤")
-  def draw_grid_line(size, :bottom), do: do_draw_grid_line(size, "└", "┴", "┘")
+  @top_line_set    {"┌", "┬", "┐"}
+  @middle_line_set {"├", "┼", "┤"}
+  @bottom_line_set {"└", "┴", "┘"}
 
-  @spec do_draw_grid_line(pos_integer, String.t, String.t, String.t) :: String.t
-
-  defp do_draw_grid_line(1, first, _, last) do
-    Enum.join([first, "───", last])
+  @spec draw_grid_line(pos_integer, :top | :middle | :bottom, pos_integer) :: String.t
+  def draw_grid_line(count, line_set, item_width \\ 3)
+  def draw_grid_line(count, :top, item_width) do
+    do_draw_grid_line(count, @top_line_set, item_width)
+  end
+  def draw_grid_line(count, :middle, item_width) do
+    do_draw_grid_line(count, @middle_line_set, item_width)
+  end
+  def draw_grid_line(count, :bottom, item_width) do
+    do_draw_grid_line(count, @bottom_line_set, item_width)
   end
 
-  defp do_draw_grid_line(size, first, inner, last) do
-    middle = for _ <- 0..size-2, do: inner
+  @spec do_draw_grid_line(pos_integer, {String.t, String.t, String.t}, pos_integer) :: String.t
+  defp do_draw_grid_line(count, line_set, item_width)
+  defp do_draw_grid_line(1, line_set, item_width) do
+    first = elem(line_set, 0)
+    last  = elem(line_set, 2)
+    spacer = String.duplicate("─", item_width)
+
+    Enum.join([first, spacer, last])
+  end
+  defp do_draw_grid_line(count, line_set, item_width) do
+    first = elem(line_set, 0)
+    inner = elem(line_set, 1)
+    last  = elem(line_set, 2)
+    spacer = String.duplicate("─", item_width)
+
+    middle = for _ <- 0..count-2, do: inner
     separators = [first] ++ middle ++ [last]
-    Enum.join(separators, "───")
+
+    Enum.join(separators, spacer)
   end
 
   @spec draw_segments([String.t], String.t) :: String.t
