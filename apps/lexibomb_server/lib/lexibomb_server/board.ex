a3db208b6cb9183a91fc26878da50edbac77c0f9
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-01-30 22:32:09 -0800

    Do use get_and_update with the hidden __reveal__/2 function
    Reverting the last change to this function since it is special case
    meant for debugging; I might as well return the new board after I've
    updated the Agent...it will always save me a call to Board.get/1.

--- apps/lexibomb_server/lib/lexibomb_server/board.ex
+++ apps/lexibomb_server/lib/lexibomb_server/board.ex
@@ -154,10 +154,11 @@ defmodule LexibombServer.Board do
 
   # Reveal all the squares on a `board` for debugging.
   @doc false
-  @spec __reveal__(t | pid) :: :ok | t
+  @spec __reveal__(pid | t) :: t
   def __reveal__(pid) when is_pid(pid) do
-    Agent.update(pid, fn board ->
-      __reveal__(board)
+    Agent.get_and_update(pid, fn board ->
+      new_board = __reveal__(board)
+      {new_board, new_board}
     end)
   end
   def __reveal__(board) do
