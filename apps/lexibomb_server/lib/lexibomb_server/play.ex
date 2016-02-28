e69ebf571b7021af8b03d00e1099b07b3d6b2a42
Aaron Bull Schaefer <aaron@elasticdog.com>
2016-02-28 11:50:26 -0800

    Implement crossword functionality

--- apps/lexibomb_server/lib/lexibomb_server/play.ex
+++ apps/lexibomb_server/lib/lexibomb_server/play.ex
@@ -9,6 +9,7 @@ defmodule LexibombServer.Play do
   alias LexibombServer.Board.Square
   alias LexibombServer.Rack
 
+  @type cardinal_direction :: :n | :e | :s | :w
   @type direction :: :across | :down
   @type t :: %{
     start: Board.coord,
@@ -39,15 +40,85 @@ defmodule LexibombServer.Play do
 
   defp do_letters_with_coords(acc, _, [], _), do: Enum.reverse(acc)
 
-  @spec next_coord(Grid.coord, direction) :: Grid.coord
+  @spec next_coord(Grid.coord, cardinal_direction | direction) :: Grid.coord
   defp next_coord(coord, direction)
-  defp next_coord({row, col}, :across), do: {row, col + 1}
-  defp next_coord({row, col}, :down), do: {row + 1, col}
+  defp next_coord(coord, :across), do: next_coord(coord, :e)
+  defp next_coord(coord, :down), do: next_coord(coord, :s)
+  defp next_coord({row, col}, :n), do: {row - 1, col}
+  defp next_coord({row, col}, :e), do: {row, col + 1}
+  defp next_coord({row, col}, :s), do: {row + 1, col}
+  defp next_coord({row, col}, :w), do: {row, col - 1}
+
+  @doc """
+  Returns the word that intersects `coord` in the perpendicular direction to
+  the given `direction`.
+
+  Uses '.' for the one square that is missing a letter.
+  """
+  @spec crossword(Grid.t, Grid.coord, direction) :: String.t
+  def crossword(grid, coord, direction) do
+    dir = perpendicular(direction)
+    {first_r, first_c} = scan_letters(grid, coord, opposite(dir))
+    {last_r, last_c} = scan_letters(grid, coord, dir)
+
+    coords =
+      for row <- first_r..last_r,
+          col <- first_c..last_c,
+          do: {row, col}
+
+    coords_to_letters(grid, coords)
+  end
+
+  # Returns the word made up of the tiles on the given coordinates.
+  #
+  # Uses '.' for any missing letters.
+  @spec coords_to_letters(Grid.t, [Grid.coord]) :: String.t
+  defp coords_to_letters(grid, coords) do
+    coords
+    |> Enum.map(&coord_to_letter(grid, &1))
+    |> Enum.join
+  end
+
+  # Returns the letter of the tile on the given coordinate.
+  #
+  # Uses '.' for a missing letter.
+  @spec coord_to_letter(Grid.t, Grid.coord) :: String.t
+  defp coord_to_letter(grid, coord) do
+    square = Map.get(grid, coord)
+
+    case square.tile do
+      "" -> "."
+      _ -> square.tile
+    end
+  end
+
+  # Returns the cardinal direction that is perpendicular to the given
+  # `direction`.
+  @spec perpendicular(direction) :: :e | :s
+  defp perpendicular(direction) do
+    case direction do
+      :across -> :s
+      :down -> :e
+    end
+  end
+
+  @doc """
+  Returns the cardinal direction that is opposite to the given `direction`.
+  """
+  @spec opposite(cardinal_direction) :: cardinal_direction
+  def opposite(direction) do
+    case direction do
+      :n -> :s
+      :e -> :w
+      :s -> :n
+      :w -> :e
+    end
+  end
 
   # Returns the coordinate of the last square starting from `coord` and going
   # in `direction` that is a letter.
-  @spec scan_letters(Grid.t, Grid.coord, direction) :: Grid.coord
-  defp scan_letters(grid, coord, direction) do
+  @spec scan_letters(Grid.t, Grid.coord, cardinal_direction | direction) :: Grid.coord
+  def scan_letters(grid, coord, direction) do
     next_coord = next_coord(coord, direction)
     next_square = Map.get(grid, next_coord)
 
@@ -60,8 +131,8 @@ defmodule LexibombServer.Play do
 
   # Returns the coordinate of the last square starting from `coord` and going
   # in `direction` that is not an anchor (nor off the board).
-  @spec scan_to_anchor(Grid.t, Grid.cood, direction) :: Grid.coord
-  defp scan_to_anchor(grid, coord, direction) do
+  @spec scan_to_anchor(Grid.t, Grid.coord, cardinal_direction | direction) :: Grid.coord
+  def scan_to_anchor(grid, coord, direction) do
     next_coord = next_coord(coord, direction)
 
     cond do
